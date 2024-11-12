74767e6a1d966811fe06646ac3324f229a8dd40d
Kingamir <nafaskingamir@gmail.com>
2024-11-12 08:02:16 +0430

    Add files via upload

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '93744215959'
-global.nomerowner = ["93744215959"]
+global.owner = '+93744215959'
+global.nomerowner = ["+93744215959"]
 
 // Apikey 
 
